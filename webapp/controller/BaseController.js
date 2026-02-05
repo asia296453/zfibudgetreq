209dfada7f51dd5a967cd6667c3a6016b97e6633
Asia Taheer <85d5aba0-0dd9-4eed-b89a-fdafceaa7a0f>
2026-02-05 08:20:31 +0000

    commitcommit

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -199,7 +199,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History", "sap
         onSearchFundsCenter: function (oEvent) {
             var sValue = oEvent.getParameter("value");
             var oFilter = new sap.ui.model.Filter("Fictr", sap.ui.model.FilterOperator.EQ, sValue);
-            var oFilter1 = new sap.ui.model.Filter("Mctxt", sap.ui.model.FilterOperator.EQ, this.suser); 
+            var oFilter1 = new sap.ui.model.Filter("Mctxt", sap.ui.model.FilterOperator.EQ, this.getOwnerComponent().getModel("create").getData().results.Pernr); 
             this.getOdata("/FCTEXTSet","FundsCenter", [oFilter,oFilter1]);
         },
         onSearchCommitmentItem: function (oEvent) {
